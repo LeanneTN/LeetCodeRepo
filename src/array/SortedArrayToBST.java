2b60e8a7eeafcdacb939d806bb0070f01b11820c
LeanneTN <2321604632@qq.com>
2022-09-30 00:04:22 +0800

    sort array to BST, 使用递归作为生成树的方法，并保证一直将每一段中间的节点作为根节点以保证左右平衡

---
+++ src/array/SortedArrayToBST.java
@@ -0,0 +1,32 @@
+package array;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SortedArrayToBST {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return bst(nums, 0, nums.length-1);
+    }
+
+    public TreeNode bst(int[] nums, int left, int right){
+        if(left > right)
+            return null;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = bst(nums, left, mid - 1);
+        root.right = bst(nums, mid + 1, right);
+        return root;
+    }
+}
+
+
