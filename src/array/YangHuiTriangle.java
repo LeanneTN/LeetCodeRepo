0e9fcf5e6fe5075456397a9230801ab4a62bc6a1
LeanneTN <2321604632@qq.com>
2022-09-30 10:14:57 +0800

    YangHuiTriangle: 一开始想的过于复杂，想用递归解决，后面看题解发现可以用循环来做。主要考察的是对list的使用熟悉程度

---
+++ src/array/YangHuiTriangle.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class YangHuiTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        if(numRows < 1)
+            return null;
+        List<List<Integer>> tri = new ArrayList<List<Integer>>();
+        for(int i = 0; i < numRows; ++i){
+            List<Integer> row = new ArrayList<Integer>();
+            for(int j = 0; j <= i; ++j){
+                if(j == 0 || i == j){
+                    row.add(1);
+                }else{
+                    row.add(tri.get(i-1).get(j-1)+tri.get(i-1).get(j));
+                }
+            }
+            tri.add(row);
+        }
+        return tri;
+    }
+
+}
