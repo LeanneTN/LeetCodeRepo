74c79fd0786cb3b6426d6788919ed7297722c761
LeanneTN <2321604632@qq.com>
2022-10-03 23:38:32 +0800

    array rotate method2:此为力扣官方解法，当数组元素向右移动k次时，尾部k mod n个元素会移动到数组的头部，其余的元素会移动 k mod n个位置，所以先将所有元素翻转，保证尾部的k mod n个元素移动到数组头，再反转0~k mod n -1区间和 k mod n ~ n - 1区间元素

--- src/elementary/array/RotateArray.java
+++ src/elementary/array/RotateArray.java
@@ -10,4 +10,22 @@ public class RotateArray {
         }
         System.arraycopy(temp, 0, nums, 0, len);
     }
+
+    // method 2
+    public void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
