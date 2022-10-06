72d554cfcf12a135460f7438fbc71793bf52be47
LeanneTN <2321604632@qq.com>
2022-10-07 00:27:03 +0800

    contains duplicate: 自己做的这种方法无论是在空间上还是在时间上都存在很多可以优化的地方，思路简单，拿到元素之后向后遍历

---
+++ src/elementary/array/ContainsDuplicate.java
@@ -0,0 +1,13 @@
+package elementary.array;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
