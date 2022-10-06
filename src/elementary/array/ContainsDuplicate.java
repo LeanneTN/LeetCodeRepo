4f58b102cd2b273e120ab08a953e7fe8730eb0ec
LeanneTN <2321604632@qq.com>
2022-10-07 00:34:20 +0800

    contains duplicate2: 这种方法的思想在于将数组重新排序，如果相邻两元素相等则有重复。这种方法在时间和空间上有更好的表现，但是和使用哈希表的方法相比要差了一些。哈希表的主要思想是将数组中的元素一个个放入表中，如果有一个元素再放入的时候发现对应位置已经存在了数字，则说明重复，这一思想暂时不去实现

--- src/elementary/array/ContainsDuplicate.java
+++ src/elementary/array/ContainsDuplicate.java
@@ -1,13 +1,24 @@
 package elementary.array;
 
+import java.util.Arrays;
+
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
                 if(nums[i] == nums[j])
-                    return false;
+                    return true;
             }
         }
-        return true;
+        return false;
+    }
+
+    public boolean containsDuplicate1(int[] nums){
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i + 1])
+                return true;
+        }
+        return false;
     }
 }
