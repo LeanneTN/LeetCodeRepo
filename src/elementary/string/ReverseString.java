4b5470fffe18ebec125d110d6a6ee497fea95ecc
LeanneTN <2321604632@qq.com>
2022-10-20 14:35:10 +0800

    reverse string: 使用双指针思想

---
+++ src/elementary/string/ReverseString.java
@@ -0,0 +1,15 @@
+package elementary.string;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int i = 0, j = n - 1;
+        while(i < j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
